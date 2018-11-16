d222a4e757c2655ec8eca5b98df14a7b0217d359
LeoooY <Leoyy3@163.com>
2018-11-16 13:25:47 +0800

    update dist

---
+++ dist/precache-manifest.3362911b63925b8b786955a6554b80cd.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/GithubPagesVue/robots.txt"
+  },
+  {
+    "revision": "7ae273faa4d1be0989a7",
+    "url": "/GithubPagesVue/js/chunk-vendors.35e69482.js"
+  },
+  {
+    "revision": "476561b0602d91b70aee",
+    "url": "/GithubPagesVue/js/app.2b011cb5.js"
+  },
+  {
+    "revision": "8737bee7c896847f176dd446f7c3d7b8",
+    "url": "/GithubPagesVue/index.html"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/GithubPagesVue/img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "476561b0602d91b70aee",
+    "url": "/GithubPagesVue/css/app.5894b7d9.css"
+  }
+];
